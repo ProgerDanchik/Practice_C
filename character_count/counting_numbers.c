c6aa84a1164054f57357ba5814d8cead45f2406f
styleprog <89692175+ProgerDanchik@users.noreply.github.com>
2022-02-07 09:34:36 +0300

    Counting numbers
    Counting numbers, separator characters and other symbols

---
+++ character_count/counting_numbers.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/* counting numbers, separator characters and other symbols */
+
+main() {
+	int c, i, nwhite, nother;
+	int ndigit[10];
+
+	nwhite = nother = 0;
+
+	for (i = 0; i < 10; ++i) {
+		ndigit[i] = 0;
+	}
+	while ((c = getchar()) != EOF) {
+		if (c >= '0' && c <= '9') {
+			++ndigit[c - '0'];
+		}
+		else if (c == ' ' || c == "\n" || c == '\t') {
+			++nwhite;
+		}
+		else {
+			++nother;
+		}
+	}
+
+	printf("Numbers =");
+	for (i = 0; i < 10; ++i) {
+		printf(" %d", ndigit[i]);
+	}
+	printf(",  delimiter characters = %d, other = %d\n", nwhite, nother);
+}
